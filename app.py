d1e328e87ba8510ba46e19dea17936b923cdc936
Ricky-Case <raca226@uky.edu>
2024-09-07 08:51:21 -0400

    20240907 - Tentatively complete

--- app.py
+++ app.py
@@ -1,62 +1,71 @@
 import numpy as np
-import matplotlib.pyplot as mplib
+import matplotlib.pyplot as mpl
 
 # (Hint: import numpy and matplotlib packages, and refer to quiver https://matplotlib.org/stable/api/_as_gen/matplotlib.pyplot.quiver.html)
 
 # Visualize the addition of two input vectors "v1" and "v2" in 2D.
 def vec_add_plot(v1, v2):
-    print("Part 1\n")
+    origin = np.array([[0,0], [0,0]])
     
-    result = []
-    
-    if (len(v1) == 2 and len(v2) == 2):
-        print("Computing ", v1, " + ", v2, "...")
-        result = np.array(v1) + np.array(v2)
-    else:
+    if (len(v1) != 2 or len(v2) != 2):
         print("Error: Please enter two vectors of length 2.")
+        return
+    else:
+        print("Computing ", v1, " + ", v2, "...\n")
+    
+    vectors = np.array([v1, v2])
+    result = np.array(v1) + np.array(v2)
     
     print("Result: ", result, "\n\n")
+    print("With origin: ", origin)
     
     # Visualize the vector addition with matplotlib.
-    mplib.plot(v1, "r-", label = "Vector 1")
-    mplib.plot(v2, "r-", label = "Vector 2")
-    mplib.plot(result, "b-", label = "New Vector")
+    # mplib.plot(vectors[0], "r-", label = "Vector 1")
+    # mplib.plot(vectors[1], "b-", label = "Vector 2")
+    # mplib.plot(result, "g-", label = "Resulting Vector")
+    mpl.quiver(*origin, vectors[:,0], vectors[:,1], color = ['g', 'b'], angles = 'xy', scale_units = 'xy', scale = 1)
     
-    mplib.title("Vector Addition")
-    mplib.xlabel("X-Axis")
-    mplib.ylabel("Y-Axis")
+    mpl.quiver(*origin, result[0], result[1], angles = 'xy', scale_units = 'xy', scale = 1)
     
-    # mplib.show()
+    mpl.xlim(-4, 4)
+    mpl.ylim(-4, 4)
+    mpl.gca().set_aspect("equal")
+    
+    mpl.title("Vector Addition")
+    mpl.xlabel("X-Axis")
+    mpl.ylabel("Y-Axis")
+    
+    mpl.show()
 
 
 # Check the orthogonality of two column vectors "v1" and "v2" of the same length, using tol as a scalar value strictly larger than 0.
 def check_ortho(v1, v2, tol):
-    print("Part 2\n")
     if (len(v1) != len(v2)):
         print("Error: Vector Dimension Mismatch!\n",
               "Please enter two vectors which have equivalent dimensions.")
+        return
     elif (tol <= 0):
         print("Error: Integer provided must be breater than 0.")
+        return
+
+    cv1 = np.array(v1).T
+    cv2 = np.array(v2).T
+    
+    print("Column Vector 1:\n", cv1, "\n")
+    print("Column Vector 2:\n", cv2, "\n")
+    
+    if((np.dot(cv1, cv2) < (0 - tol)) or (np.dot(cv1, cv2) > (0 + tol))):
+        return False
     else:
-        cv1 = np.array(v1).T
-        cv2 = np.array(v2).T
-        
-        print("Column Vector 1:\n", cv1, "\n")
-        print("Column Vector 2:\n", cv2, "\n")
-        
-        # theta = np.arccos(np.dot(cv1, cv2)/(np.linalg.norm(cv1) * np.linalg.norm(cv2)))
-        # checkValue = np.abs(np.pi/2 - theta)
-        
-        if((np.dot(cv1, cv2) < (0 - tol)) or (np.dot(cv1, cv2) > (0 + tol))):
-            return False
-        else:
-            return True
+        return True
 
 
 ################
 #### PART 1 ####
 ################
 
+
+print("Part 1\n\n")
 
 vector1 = [4, 2]
 vector2 = [-1, 2]
@@ -67,15 +76,16 @@ vec_add_plot(vector1, vector2)
 #### PART 2 ####
 ################
 
+print("Part 2\n\n")
 
 columnVector1 = [5, 4, 4, 5, 1, 4, 1]
 columnVector2 = [-1, -2, 1, -2, 3, 1, -5]
 tolVal = 0.001
 
 if(check_ortho(columnVector1, columnVector2, tolVal)):
-    print("These vectors are orthogonal.\n")
+    print("1 - These vectors are orthogonal.\n")
 else:
-    print("These vectors are not orthogonal.\n")
+    print("0 - These vectors are not orthogonal.\n")
 
 
 columnVector1 = [2, 18]
@@ -92,6 +102,7 @@ else:
 #### PART 3 ####
 ################
 
+print("Part 3\n\n")
 
 # Part 3 (2pt)
 # - Read the data set A.csv as a matrix A ∈ R^(30 * 6) via:
@@ -100,15 +111,25 @@ else:
 A = np.genfromtxt("A.csv", delimiter=',')
 
 # 1. Compute the SVD of A...
-svdA = np.linalg.svd(A)
-
-s = np.linalg.svd([[1, 2, 3],
-                   [2, 4, 6],
-                   [-1, 1, -1]])
+tup1, tup2, tup3 = np.linalg.svd(np.array(A))
 
 
 # ...And find:
 # 2. The third right singular vector
+print("THIRD RIGHT SINGULAR VECTOR: ")
+print(tup1[-3])
+print("\n\n")
+
 # 3. The second singular value
+print("SECOND SINGULAR VALUE: ")
+print(tup2[1])
+print("\n\n")
+
 # 4. The fourth left singular vector
+print("FOURTH LEFT SINGULAR VECTOR: ")
+print(tup1[3])
+print("\n\n")
+
 # 5. The rank of A
+print("Rank of matrix: ")
+print(np.linalg.matrix_rank(np.array(A)))
