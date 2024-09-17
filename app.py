424565f2738b244ab9c117bdfefba71fa4495e38
Ricky-Case <raca226@uky.edu>
2024-09-16 20:51:37 -0400

    20240916 - Project Complete

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as mpl
 
-# (Hint: import numpy and matplotlib packages, and refer to quiver https://matplotlib.org/stable/api/_as_gen/matplotlib.pyplot.quiver.html)
+
+###################
+#### FUNCTIONS ####
+###################
+
 
 # Visualize the addition of two input vectors "v1" and "v2" in 2D.
 def vec_add_plot(v1, v2):
@@ -20,9 +24,6 @@ def vec_add_plot(v1, v2):
     print("With origin: ", origin)
     
     # Visualize the vector addition with matplotlib.
-    # mplib.plot(vectors[0], "r-", label = "Vector 1")
-    # mplib.plot(vectors[1], "b-", label = "Vector 2")
-    # mplib.plot(result, "g-", label = "Resulting Vector")
     mpl.quiver(*origin, vectors[:,0], vectors[:,1], color = ['g', 'b'], angles = 'xy', scale_units = 'xy', scale = 1)
     
     mpl.quiver(*origin, result[0], result[1], angles = 'xy', scale_units = 'xy', scale = 1)
@@ -60,9 +61,37 @@ def check_ortho(v1, v2, tol):
         return True
 
 
-################
-#### PART 1 ####
-################
+# - Read the data set A.csv as a matrix A ∈ R^(30 * 6) via:
+# > from numpy import genfromtxt
+def svd_operations(matrix):
+    # 1. Compute the SVD of A...
+    u, z, vT = np.linalg.svd(matrix)
+
+    # ...And find:
+    # 2. The third right singular vector
+    print("THIRD RIGHT SINGULAR VECTOR: ")
+    print(vT.T[2])
+    print("\n\n")
+
+    # 3. The second singular value
+    print("SECOND SINGULAR VALUE: ")
+    print(z[1])
+    print("\n\n")
+
+    # 4. The fourth left singular vector
+    print("FOURTH LEFT SINGULAR VECTOR: ")
+    print(u[3])
+    print("\n\n")
+
+    # 5. The rank of A
+    print("Rank of matrix: ")
+    print(np.linalg.matrix_rank(matrix))
+    print("\n")
+
+
+########################
+#### EXECUTE PART 1 ####
+########################
 
 
 print("Part 1\n\n")
@@ -72,9 +101,10 @@ vector2 = [-1, 2]
 vec_add_plot(vector1, vector2)
 
 
-################
-#### PART 2 ####
-################
+########################
+#### EXECUTE PART 2 ####
+########################
+
 
 print("Part 2\n\n")
 
@@ -87,7 +117,6 @@ if(check_ortho(columnVector1, columnVector2, tolVal)):
 else:
     print("0 - These vectors are not orthogonal.\n")
 
-
 columnVector1 = [2, 18]
 columnVector2 = [3/2, -1/6]
 tolVal = 0.001
@@ -98,38 +127,13 @@ else:
     print("These vectors are not orthogonal.\n")
 
 
-################
-#### PART 3 ####
-################
+########################
+#### EXECUTE PART 3 ####
+########################
 
 print("Part 3\n\n")
 
-# Part 3 (2pt)
-# - Read the data set A.csv as a matrix A ∈ R^(30 * 6) via:
-# > from numpy import genfromtxt
-
 A = np.genfromtxt("A.csv", delimiter=',')
+matrix = np.array(A)
 
-# 1. Compute the SVD of A...
-tup1, tup2, tup3 = np.linalg.svd(np.array(A))
-
-
-# ...And find:
-# 2. The third right singular vector
-print("THIRD RIGHT SINGULAR VECTOR: ")
-print(tup1[-3])
-print("\n\n")
-
-# 3. The second singular value
-print("SECOND SINGULAR VALUE: ")
-print(tup2[1])
-print("\n\n")
-
-# 4. The fourth left singular vector
-print("FOURTH LEFT SINGULAR VECTOR: ")
-print(tup1[3])
-print("\n\n")
-
-# 5. The rank of A
-print("Rank of matrix: ")
-print(np.linalg.matrix_rank(np.array(A)))
+svd_operations(matrix)
